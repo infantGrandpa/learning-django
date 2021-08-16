5e03d8665bc345b403c98e9b0b81487c9e8932f9
infantGrandpa <babraham341@gmail.com>
2021-08-16 15:47:27 -0400

    Added Post and User Data
    Created Post database
    Added Post editing to admin page
    Added several posts
    Added some users
    Removed dummy post data

--- django_tutorial/django_project/blog/views.py
+++ django_tutorial/django_project/blog/views.py
@@ -1,44 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
-
-posts = [
-    {
-        'author': 'Brett Abraham',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 11, 2021'
-    },
-    {
-        'author': 'Brett Abraham',
-        'title': 'Blog Post 2',
-        'content': '2nd Post Content',
-        'date_posted': 'August 12, 2021'
-    },
-    {
-        'author': 'Brett Abraham',
-        'title': 'Blog Post 3',
-        'content': '3rd Post Content',
-        'date_posted': 'August 13, 2021'
-    },
-    {
-        'author': 'Brett Abraham',
-        'title': 'Blog Post 4',
-        'content': '4th Post Content',
-        'date_posted': 'August 14, 2021'
-    },
-    {
-        'author': 'Brett Abraham',
-        'title': 'Blog Post 5',
-        'content': '5th Post Content',
-        'date_posted': 'August 15, 2021'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
