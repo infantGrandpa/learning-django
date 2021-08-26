2bbe8de577298de86710e63a05e9bf4b734a18f9
infantGrandpa <babraham341@gmail.com>
2021-08-26 11:58:15 -0400

    Added Pagination
    Fixed bug were registering new users would throw an error, which prevented them from creating a profile

--- django_tutorial/django_project/users/models.py
+++ django_tutorial/django_project/users/models.py
@@ -10,8 +10,8 @@ class Profile (models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
         img = Image.open(self.image.path)
         maxSize = 256
         if img.height > maxSize or img.width > maxSize:
--- django_tutorial/django_project/users/views.py
+++ django_tutorial/django_project/users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(
-                request, f'Your account {username} has been created!'
+                request, f'Your account {username} has been created! '
                 'You are now able to log in.')
             return redirect('blog-home')
     else:
